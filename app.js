73cc974731bef1ddf585eb9d61aa1df73a0ca129
Joshua <j2h5068312@gmail.com>
2023-02-14 20:08:59 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const express =require('express')
+const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
-const  port = 3000
+const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+
+//載入dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) =>{
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
   res.render('index')
 })
 
