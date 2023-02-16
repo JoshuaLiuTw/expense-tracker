a11ffef427550d05881480efe47b3202262cf489
Joshua <j2h5068312@gmail.com>
2023-02-16 21:05:06 +0800

    feat:  create and edit function added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const Record = require('./models/record')
+const bodyParser = require('body-parser')
+const record = require('./models/record')
 
 
 //載入dotenv
@@ -26,6 +28,8 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Record.find()
     .lean()
@@ -33,6 +37,40 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const name = req.body.name
+  return Record.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(error => console.log(error))
+
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
+
 app.listen(port, () => {
   console.log('Express  is listening on localhost:${port}')
 })
+
