b499c98c9e1296424062c70b1d44cbf2102be970
Joshua <j2h5068312@gmail.com>
2023-02-19 00:34:26 +0800

    feat: user login feature via passport

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 //載入dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -37,7 +39,8 @@ app.use(session({
 }))
 
 
-
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,8 +5,13 @@ const User = require('../../models/user')
 router.get('/login',(req,res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {
-})
+
+router.post('/login',passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
+
+
 router.get('/register', (req, res) => {
   res.render('register')
 })
